02cc43a0ed415884f2f195e39b204d0d2442c9ff
gabengar <gabengar@protonmail.com>
2021-12-08 19:36:57 +0500

    style: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,35 @@ import { InternalAnchour } from "#components/fancy";
 import { Section } from "#components/page";
 
 import type { NextPage } from "next";
+import { siteTitle } from "#lib/util";
 
 const Home: NextPage = () => {
+  const pageTitle = "Gabengar's site";
 
-  return (<Section heading="Next.JS site">
-    <Head>
-      <title>Next.JS site</title>
-      <meta name="description" content="The site made with Next.JS" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <nav>
-      <ul>
-        <li>
-          <InternalAnchour href="/">
-            Home
-          </InternalAnchour>
-        </li>
-        <li>
-          <InternalAnchour href="/frontend-mentor">
-            Frontend Mentor Challenges
-          </InternalAnchour>
-        </li>
-      </ul>
-    </nav>
-  </Section>);
+  return (
+    <Section heading={pageTitle}>
+      <Head>
+        <title>{siteTitle(pageTitle)}</title>
+        <meta
+          name="description"
+          content="This is the site of mine made with nextjs."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav>
+        <ul>
+          <li>
+            <InternalAnchour href="/">Home</InternalAnchour>
+          </li>
+          <li>
+            <InternalAnchour href="/frontend-mentor">
+              Frontend Mentor Challenges
+            </InternalAnchour>
+          </li>
+        </ul>
+      </nav>
+    </Section>
+  );
 };
 
 export default Home;
