f2d0edc33e5843efbd540670646809369b3cd34e
gabengar <gabengar@protonmail.com>
2021-12-08 13:32:07 +0500

    style: base layout

--- src/components/page/layout/base.tsx
+++ src/components/page/layout/base.tsx
@@ -1,21 +1,25 @@
+import { Anchour } from "#components/fancy";
 import styles from "./base.module.scss";
 
 import type { RootlessProps } from "#types";
+import { ENV_VARS } from "#configs/public";
 
-interface Props extends RootlessProps {
-}
+interface Props extends RootlessProps {}
 
 export function BaseLayout({ children }: Props) {
+  return (
+    <>
+      <header className={styles.header}>
 
-  return (<>
-    <header className={styles.header}>
-    </header>
+      </header>
 
-    <main className={styles.main}>
-      {children}
-    </main>
+      <main className={styles.main}>{children}</main>
 
-    <footer className={styles.footer}>
-    </footer>
-  </>);
+      <footer className={styles.footer}>
+        <Anchour href={ENV_VARS.REPOSITORY}>
+          Source code
+        </Anchour>
+      </footer>
+    </>
+  );
 }
