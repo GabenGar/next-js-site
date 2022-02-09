f8f44c9c083dce1f39cc12e73930c017b58f1a2e
gabengar <gabengar@protonmail.com>
2022-02-09 12:03:07 +0500

    feat: recursive freeze

---
+++ src/lib/util/deep-freeze.ts
@@ -0,0 +1,23 @@
+/**
+ * Freezes an object recusively.
+ * @param obj
+ */
+export function deepFreeze(obj: Record<string, unknown>) {
+  // Retrieve the property names defined on object
+  const propNames = Object.getOwnPropertyNames(obj);
+
+  // Freeze properties before freezing self
+  for (const name of propNames) {
+    const value = obj[name];
+
+    if (!value) {
+      continue;
+    }
+
+    if (typeof value === "object") {
+      deepFreeze(value as Record<string, unknown>);
+    }
+  }
+
+  return Object.freeze(obj);
+}
