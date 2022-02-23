b2f07b19a03c7966d171192aeaf121104232a3e0
gabengar <gabengar@protonmail.com>
2022-02-23 12:48:05 +0500

    feat(server): get notes API

---
+++ src/pages/api/v1/account/calendar/get-month-notes.ts
@@ -0,0 +1,65 @@
+import {
+  UNPROCESSABLE_ENTITY,
+  OK,
+  INTERNAL_SERVER_ERROR,
+  NOT_AUTHORIZED,
+} from "#environment/constants/http";
+import { getAccountDetails, withSessionRoute } from "#lib/account";
+import { getCalendarNotesForMonth } from "#database/queries/account/calendar";
+import type { APIRequest, APIResponse } from "#types/api";
+import type { ICalendarNote } from "#types/entities";
+import { fromISOString, IISODateString } from "#lib/dates";
+
+interface RequestBody extends APIRequest<{ date: IISODateString }> {}
+
+export default withSessionRoute<APIResponse<ICalendarNote[]>>(
+  async (req, res) => {
+    if (req.method === "POST") {
+      const { account_id } = req.session;
+
+      if (!account_id) {
+        return res
+          .status(NOT_AUTHORIZED)
+          .json({ success: false, errors: ["Not Authorized."] });
+      }
+
+      const account = await getAccountDetails(account_id);
+
+      if (!account) {
+        const reqDetails = JSON.stringify(req, undefined, 2);
+        console.error(
+          `Account with ID "${account_id}" doesn't exist.\n`,
+          `Request details: "${reqDetails}".`
+        );
+        req.session.destroy();
+
+        return res.status(INTERNAL_SERVER_ERROR).json({
+          success: false,
+          errors: ["Unknown Error."],
+        });
+      }
+      const { data }: RequestBody = req.body;
+      const isValidBody = [
+        data,
+        "date" in data,
+        typeof data.date === "string",
+      ].every((value) => value);
+
+      if (!isValidBody) {
+        return res.status(UNPROCESSABLE_ENTITY).json({
+          success: false,
+          errors: ["Invalid body."],
+        });
+      }
+
+      const { date } = data;
+
+      const notes = await getCalendarNotesForMonth(
+        account_id,
+        fromISOString(date)
+      );
+
+      return res.status(OK).json({ success: true, data: notes });
+    }
+  }
+);
