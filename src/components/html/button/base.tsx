370f93139085ab50fd2ac347b918f7a53967ed43
gabengar <gabengar@protonmail.com>
2022-02-28 00:55:59 +0500

    feat(client): button icons

--- src/components/html/button/base.tsx
+++ src/components/html/button/base.tsx
@@ -1,13 +1,21 @@
 import { blockComponent } from "#components/meta";
+import { SVGIcon } from "#components/icons";
 import styles from "./_index.module.scss";
 
 import type { BlockProps } from "#types/props";
 
-export interface HTMLButtonProps extends BlockProps<"button"> {}
+export interface HTMLButtonProps extends BlockProps<"button"> {
+  iconID?: string;
+}
 
 export const HTMLButton = blockComponent<HTMLButtonProps>(
   styles.block,
-  ({ children, ...blockProps }) => {
-    return <button {...blockProps}>{children}</button>;
+  ({ iconID, children, ...blockProps }) => {
+    return (
+      <button {...blockProps}>
+        {iconID && <SVGIcon className={styles.icon} iconID={iconID} />}
+        <span>{children}</span>
+      </button>
+    );
   }
 );
