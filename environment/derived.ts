60ccf67ce883011216e04a9e5d5d48637b9662fc
gabengar <gabengar@protonmail.com>
2022-03-15 19:40:29 +0500

    fix: public vars

--- environment/derived.ts
+++ environment/derived.ts
@@ -4,3 +4,5 @@ export const IS_DEVELOPMENT = NODE_ENV === "development";
 export const IS_BROWSER = typeof window !== "undefined";
 export const IS_INVITE_ONLY = Boolean(IS_INVITE_ONLY_String);
 export const IS_PUBLIC = Boolean(IS_PUBLIC_String);
+
+console.log(IS_INVITE_ONLY_String, IS_PUBLIC_String, IS_INVITE_ONLY, IS_PUBLIC);
--- environment/vars.ts
+++ environment/vars.ts
@@ -3,6 +3,8 @@ export const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME!;
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN!;
 export const REPOSITORY = process.env.NEXT_PUBLIC_REPOSITORY!;
 export const EMAIL_ADDRESS = process.env.NEXT_PUBLIC_EMAIL_ADDRESS!;
+export const IS_PUBLIC_String = process.env.NEXT_PUBLIC_IS_PUBLIC!;
+export const IS_INVITE_ONLY_String = process.env.NEXT_PUBLIC_IS_INVITE_ONLY!;
 export const DATABASE_HOSTNAME = process.env.DATABASE_HOSTNAME!;
 export const DATABASE_PORT = process.env.DATABASE_PORT!;
 export const DATABASE_NAME = process.env.DATABASE_NAME!;
@@ -14,5 +16,4 @@ export const EMAIL_SMTP_HOSTNAME = process.env.EMAIL_SMTP_HOSTNAME!;
 export const EMAIL_PORT = process.env.EMAIL_PORT!;
 export const EMAIL_USERNAME = process.env.EMAIL_USERNAME!;
 export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD!;
-export const IS_PUBLIC_String = process.env.IS_PUBLIC!;
-export const IS_INVITE_ONLY_String = process.env.IS_INVITE_ONLY!;
+
