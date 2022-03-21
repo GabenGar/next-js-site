c191436d596af871969d6d6ff047d566a94774fc
gabengar <gabengar@protonmail.com>
2022-03-21 14:05:07 +0500

    feat: invite card

--- src/pages/account/admin/invites/index.tsx
+++ src/pages/account/admin/invites/index.tsx
@@ -3,10 +3,12 @@ import { siteTitle } from "#lib/util";
 import { getAccountDetails, withSessionSSR } from "#lib/account";
 import { getInvites } from "#database/queries/account/admin";
 import { Page } from "#components/pages";
-import { JSONView } from "#components/json";
-import { CardList } from "#components/lists";
-import { Card, CardBody } from "#components/cards";
 import { Nav, NavList } from "#components/navigation";
+import { CardList } from "#components/lists";
+import { DL, DS, DT, DD } from "#components/lists/d-list";
+import { Card, CardBody, CardHeader, CardFooter } from "#components/cards";
+import { Heading } from "#components/headings";
+import { DateTimeView } from "#components/dates";
 import { LinkInternal } from "#components/links";
 
 import type { InferGetServerSidePropsType } from "next";
@@ -38,9 +40,65 @@ function InvitesPage({
       <CardList>
         {invites.map((invite) => (
           <Card key={invite.code}>
+            <CardHeader>
+              <Heading level={2}>
+                <code>{invite.code}</code>
+              </Heading>
+            </CardHeader>
             <CardBody>
-              <JSONView json={invite} />
+              <DL>
+                <DS>
+                  <DT>Status</DT>
+                  {/* @TODO styling */}
+                  <DD>{invite.is_active ? "active" : "inactive"}</DD>
+                </DS>
+                <DS
+                  dKey={"Created at"}
+                  dValue={<DateTimeView dateTime={invite.created_at} />}
+                />
+                <DS
+                  dKey={"Expires at"}
+                  dValue={
+                    invite.expires_at ? (
+                      <DateTimeView dateTime={invite.expires_at} />
+                    ) : (
+                      "Never"
+                    )
+                  }
+                />
+                <DS
+                  dKey={"Maximum uses"}
+                  dValue={invite.max_uses ?? "Unlimited"}
+                />
+              </DL>
             </CardBody>
+            <CardFooter style={{ textAlign: "center" }}>
+              <Nav>
+                {/* @TODO: endpoints */}
+                <NavList>
+                  <LinkInternal
+                    href={{
+                      pathname: "/account/admin/account/[account_id]",
+                      query: {
+                        account_id: invite.account_id,
+                      },
+                    }}
+                  >
+                    Creator details
+                  </LinkInternal>
+                  <LinkInternal
+                    href={{
+                      pathname: "/account/admin/invite/[invite_id]",
+                      query: {
+                        invite_id: invite.id,
+                      },
+                    }}
+                  >
+                    Invite details
+                  </LinkInternal>
+                </NavList>
+              </Nav>
+            </CardFooter>
           </Card>
         ))}
       </CardList>
