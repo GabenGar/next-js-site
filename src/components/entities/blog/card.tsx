935f8fcac24d378997491268ffa7a7ed6dff4aed
gabengar <gabengar@protonmail.com>
2022-03-25 18:34:03 +0500

    feat(tl): blog card

--- src/components/entities/blog/card.tsx
+++ src/components/entities/blog/card.tsx
@@ -1,6 +1,8 @@
+import { useTranslation } from "next-i18next";
 import { blockComponent } from "#components/meta";
 import { Card, CardBody, CardHeader, CardFooter } from "#components/cards";
 import { Heading } from "#components/headings";
+import { DateTimeView } from "#components/dates";
 import { LinkInternal } from "#components/links";
 import { DL, DS } from "#components/lists/d-list";
 import styles from "./card.module.scss";
@@ -14,28 +16,35 @@ export interface IBlogPostProps extends ICardProps {
 
 export const BlogPostCard = blockComponent<IBlogPostProps>(
   styles.block,
-  ({ post, headingLevel = 2, ...blockProps }) => {
-    return (
-      <Card {...blockProps}>
-        <CardHeader>
-          <Heading level={headingLevel}>{post.title}</Heading>
-          <DL className={styles.meta}>
-            <DS dKey={"By"} dValue={post.author} />
-            <DS dKey={"Published"} dValue={post.created_at} />
-          </DL>
-        </CardHeader>
-        <CardBody>{post.excerpt}</CardBody>
-        <CardFooter>
-          <LinkInternal
-            href={{
-              pathname: "/blog/[slug]",
-              query: { slug: post.slug },
-            }}
-          >
-            Read more
-          </LinkInternal>
-        </CardFooter>
-      </Card>
-    );
-  }
+  Component
 );
+
+function Component({ post, headingLevel = 2, ...blockProps }: IBlogPostProps) {
+  const { t } = useTranslation("components");
+
+  return (
+    <Card {...blockProps}>
+      <CardHeader>
+        <Heading level={headingLevel}>{post.title}</Heading>
+        <DL className={styles.meta}>
+          <DS dKey={t("blog_by")} dValue={post.author} />
+          <DS
+            dKey={t("blog_published")}
+            dValue={<DateTimeView dateTime={post.created_at} />}
+          />
+        </DL>
+      </CardHeader>
+      <CardBody>{post.excerpt}</CardBody>
+      <CardFooter>
+        <LinkInternal
+          href={{
+            pathname: "/blog/[slug]",
+            query: { slug: post.slug },
+          }}
+        >
+          {t("blog_read_more")}
+        </LinkInternal>
+      </CardFooter>
+    </Card>
+  );
+}
