e4d5b7ae2d1aa498c88f794193e06a510e40952a
gabengar <gabengar@protonmail.com>
2022-04-11 20:25:32 +0500

    chore: components cleanup

--- src/components/pages/_index.ts
+++ src/components/pages/_index.ts
@@ -1,5 +1,2 @@
-export { Section } from "./section";
-export { Header } from "./header";
-export { Heading } from "./heading";
 export { Page } from "./base"
 export type { PageProps } from "./base"
