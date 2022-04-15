f8ee584d5cd935efb2cd3e4c32cd36073c264300
gabengar <gabengar@protonmail.com>
2022-04-15 22:07:37 +0500

    refactor: comments db schema

--- src/database/migrations/20220413184311746_comments.sql
+++ src/database/migrations/20220413184311746_comments.sql
@@ -1,16 +1,34 @@
 -- Up Migration
-CREATE TABLE comments (
+CREATE SCHEMA comments;
+
+CREATE TABLE comments.entries (
   id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   account_id int NOT NULL REFERENCES accounts,
-  parent_id int REFERENCES comments,
-  blog_slug text NOT NULL,
+  parent_id int REFERENCES comments.entries,
   content text,
   is_public boolean NOT NULL DEFAULT FALSE
 );
 
-CREATE INDEX ON comments (created_at);
-CREATE INDEX ON comments (blog_slug);
-CREATE INDEX ON comments (parent_id);
-CREATE INDEX ON comments (is_public);
+CREATE INDEX ON comments.entries (created_at);
+
+CREATE INDEX ON comments.entries (created_at);
+
+CREATE INDEX ON comments.entries (parent_id);
+
+CREATE INDEX ON comments.entries (is_public);
+
+CREATE TABLE comments.blog (
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  comment_id int NOT NULL REFERENCES comments.entries,
+  blog_slug text NOT NULL
+);
+
+CREATE INDEX ON comments.blog (created_at);
+
+CREATE INDEX ON comments.blog (comment_id);
+
+CREATE INDEX ON comments.blog (blog_slug);
+
 -- Down Migration
