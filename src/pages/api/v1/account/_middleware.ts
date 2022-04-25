69a0cc6603b4ca5140a1722b13b19ceef56b5695
gabengar <gabengar@protonmail.com>
2022-04-25 19:47:43 +0500

    feat(server): api middlewares

--- src/pages/api/v1/account/_middleware.ts
+++ src/pages/api/v1/account/_middleware.ts
@@ -1,20 +1,32 @@
 import { NextResponse } from "next/server";
-import { FOUND } from "#environment/constants/http";
+import { UNAUTHORIZED } from "#environment/constants/http";
 import { SESSION_COOKIE } from "#environment/derived";
 
 import type { NextFetchEvent, NextRequest } from "next/server";
 
-export default function authMiddleware(req: NextRequest, ev: NextFetchEvent) {
+export default function apiAuthMiddleware(
+  req: NextRequest,
+  ev: NextFetchEvent
+) {
   // check for presence of the session cookie
   // redirect to the auth page if the site isn't public
   // eventually it's going to perform a full auth check
   const cookies = req.cookies[SESSION_COOKIE];
 
   if (!cookies) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/auth/login";
-    
-    return NextResponse.redirect(url, FOUND);
+    const res = new NextResponse(
+      JSON.stringify({
+        is_successful: false,
+        errors: ["Not Authorized"],
+      }),
+      {
+        status: UNAUTHORIZED,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return res;
   }
 
   return NextResponse.next();
