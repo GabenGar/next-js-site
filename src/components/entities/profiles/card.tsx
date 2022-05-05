d8d7dad3b138c6ca66c89edd1746f0ef3ae727e4
gabengar <gabengar@protonmail.com>
2022-05-05 20:22:09 +0500

    feat(client): profile card

---
+++ src/components/entities/profiles/card.tsx
@@ -0,0 +1,61 @@
+import { useTranslation } from "next-i18next";
+import { blockComponent } from "#components/meta";
+import { Card, CardBody, CardHeader, CardFooter } from "#components/cards";
+import { Heading } from "#components/headings";
+import { DateTimeView } from "#components/dates";
+import { LinkInternal } from "#components/links";
+import { DL, DS } from "#components/lists/d-list";
+import styles from "./card.module.scss";
+
+import type { IAccountProfileClient } from "#types/entities";
+import type { ICardProps } from "#components/cards";
+
+export interface IProfileCardProps extends ICardProps {
+  profile: IAccountProfileClient;
+}
+
+export const ProfileCard = blockComponent<IProfileCardProps>(
+  styles.block,
+  Component
+);
+
+function Component({
+  profile,
+  headingLevel = 2,
+  ...blockProps
+}: IProfileCardProps) {
+  const { t } = useTranslation("components");
+  const { id, created_at, updated_at, full_name } = profile;
+
+  return (
+    <Card {...blockProps}>
+      <CardHeader>
+        <Heading level={headingLevel}>
+          {full_name ?? t("profile_anonymous")}
+        </Heading>
+      </CardHeader>
+      <CardBody>
+        <DL>
+          <DS
+            dKey={t("profile_joined")}
+            dValue={<DateTimeView dateTime={created_at} />}
+          />
+          <DS
+            dKey={t("profile_last_activity")}
+            dValue={<DateTimeView dateTime={updated_at} />}
+          />
+        </DL>
+      </CardBody>
+      <CardFooter>
+        <LinkInternal
+          href={{
+            pathname: "/profile/[id]",
+            query: { slug: id },
+          }}
+        >
+          Details
+        </LinkInternal>
+      </CardFooter>
+    </Card>
+  );
+}
