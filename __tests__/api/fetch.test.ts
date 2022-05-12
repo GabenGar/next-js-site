c0675e43aa90ed6aad0a94fbcc1524f7bf6a4030
gabengar <gabengar@protonmail.com>
2022-05-13 01:23:27 +0500

    chore: reorganize tests

--- __tests__/api/fetch.test.ts
+++ __tests__/api/fetch.test.ts
@@ -1,7 +1,7 @@
 import { validateYaDiskDiskFields } from "#codegen/schema/validations";
 import { fetchDisk } from "#server/storage/ya-disk";
 
-describe("Yandex.Disk endpoints", () => {
+describe("Yandex.Disk API", () => {
   it("fetches disk", async () => {
     const yaDiskData = await fetchDisk();
     const validationResult = await validateYaDiskDiskFields(yaDiskData);
